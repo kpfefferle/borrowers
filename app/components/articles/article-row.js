033214a2a03beecb9eefbf8211f170ae2217c250
Kevin Pfefferle <kevin@pfefferle.co>
2015-03-19 17:59:34 -0400

    Allow Article state to auto-save when updated

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -4,9 +4,16 @@ export default Ember.Component.extend({
   tagName: 'tr',
   article: null,
   articleStates: null,
-  actions: {
-    saveArticle: function(article) {
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
       this.sendAction('save', article);
     }
-  }
+  },
+  stateChanged: function() {
+    var article = this.get('article');
+    if (article.get('isDirty') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  }.on('init').observes('article.state')
 });
