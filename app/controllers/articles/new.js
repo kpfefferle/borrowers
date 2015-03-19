90f834fe8cbe2cfdc8097e03e86df41261460011
Kevin Pfefferle <kevin@pfefferle.co>
2015-03-18 20:41:02 -0400

    Validate Articles on create

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  hasDescription: Ember.computed.notEmpty('model.description'),
+  hasNotes: Ember.computed.notEmpty('model.notes'),
+  isValid: Ember.computed.and('hasDescription', 'hasNotes'),
+  actions: {
+    save: function() {
+      debugger;
+      if (this.get('isValid')) {
+        return true;
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+
+        return false;
+      }
+    }
+  }
+});
