58005bc1c581b9c5095171fec27cd28afbd3ae73
Kevin Pfefferle <kevin@pfefferle.co>
2015-03-21 21:55:03 -0400

    Add example collection observers

--- app/controllers/articles/index.js
+++ app/controllers/articles/index.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  possibleStates: ["borrowed", "returned"]
+  possibleStates: ["borrowed", "returned"],
+  contentDidChange: function() {
+    console.log('Called when we add or remove an article.');
+  }.observes('model.[]'),
+  stateDidChange: function() {
+    console.log('Callen when the state property changes for any of the articles.');
+  }.observes('model.@each.state')
 });
